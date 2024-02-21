3c38a248b2f476caabb930b5f27059a4cf5ae78d
Raymond McDonald <contact@wreckingballpublishing.com>
2024-02-21 17:23:49 -0600

    Create new level with new MovingHazard object which uses tweens to move on a track

---
+++ scenes/hazards/MovingHazard.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class MovingHazard : AnimatableBody3D
+{
+    [Export]
+    private Vector3 Destination {get; set;} = new(0, 0, 0);
+    [Export]
+    private float TravelDuration {get; set;} = 1.0F;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Tween tween = CreateTween();
+        // Set tween to loop infinitely; note that I use Node.CreateTween() so tweens live just as long as the node they are bound to
+        tween.SetLoops();
+        tween.SetTrans(Tween.TransitionType.Cubic);
+        tween.TweenProperty(this, "global_position", GlobalPosition + Destination, TravelDuration);
+        tween.TweenProperty(this, "global_position", GlobalPosition, TravelDuration);
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
+}
