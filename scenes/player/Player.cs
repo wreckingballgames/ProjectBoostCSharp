fdffc616a51bc9c3c8bbde49dbe55b24e34c0278
Raymond McDonald <contact@wreckingballpublishing.com>
2024-02-21 02:12:15 -0600

    Rename RotationSpeed to TorqueThrust; export range for Thrust

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -5,8 +5,8 @@ using System.Runtime.CompilerServices;
 public partial class Player : RigidBody3D
 {
     [Export]
-    private float RotationSpeed {get; set;} = 100.0F;
-    [Export]
+    private float TorqueThrust {get; set;} = 100.0F;
+    [Export(PropertyHint.Range, "750.0F, 3000.0F")]
     private float Thrust {get; set;} = 1000.0F;
 
     public override void _Ready()
@@ -27,11 +27,11 @@ public partial class Player : RigidBody3D
 
         if (Input.IsActionPressed("rotate_left"))
         {
-            ApplyTorque(new(0.0F, 0.0F, RotationSpeed * deltaAsFloat));
+            ApplyTorque(new(0.0F, 0.0F, TorqueThrust * deltaAsFloat));
         }
         else if (Input.IsActionPressed("rotate_right"))
         {
-            ApplyTorque(new(0.0F, 0.0F, -RotationSpeed * deltaAsFloat));
+            ApplyTorque(new(0.0F, 0.0F, -TorqueThrust * deltaAsFloat));
         }
 
         base._Process(delta);
