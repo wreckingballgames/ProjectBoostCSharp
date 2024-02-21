cefe7a27422305917b82649d4cee75f2cbec5a3e
Raymond McDonald <contact@wreckingballpublishing.com>
2024-02-21 01:29:33 -0600

    Modify Player to use physics forces for boosting and rotation

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -2,25 +2,26 @@ using Godot;
 using System;
 using System.Runtime.CompilerServices;
 
-public partial class Player : Node3D
+public partial class Player : RigidBody3D
 {
-    private float RotationSpeed {get; set;} = 0.05F;
+    private float RotationSpeed {get; set;} = 100F;
 
     public override void _Process(double delta)
     {
+        float deltaAsFloat = (float)delta;
+
         if (Input.IsActionPressed("boost"))
         {
-            float y = Position.Y + (float)delta;
-            Position = Position with {Y = y};
+            ApplyCentralForce(Godot.Vector3.Up * deltaAsFloat * 1000.0F);;
         }
 
         if (Input.IsActionPressed("rotate_left"))
         {
-            RotateZ(RotationSpeed);
+            ApplyTorque(new(0.0F, 0.0F, RotationSpeed * deltaAsFloat));
         }
         else if (Input.IsActionPressed("rotate_right"))
         {
-            RotateZ(-RotationSpeed);
+            ApplyTorque(new(0.0F, 0.0F, -RotationSpeed * deltaAsFloat));
         }
 
         base._Process(delta);
